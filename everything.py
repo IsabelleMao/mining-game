970b4bf8bea631379acafc1654f0b3207fa56e28
Isabelle <isabellemao@users.noreply.github.com>
2017-03-06 20:52:48 -0500

    Added a comment for yet nonexistent code
    .

--- everything.py
+++ everything.py
@@ -1,3 +1,7 @@
+#Complete or add to the nonexistent code under any comment, or add a new comment!
+
+
+#Enter the game
 while True:
   enter = input("""Type start to begin the game!
   """)
@@ -5,4 +9,20 @@ while True:
   if enter == "start":
     break
     
-
+while True:
+  #Get the name of the user.
+  
+while True:
+  #Tutorial
+  
+  #Let the player view his/her/its info
+  
+  #Mine
+  
+  #Sell things?
+  
+  #Buy a mine
+  
+  #Buy (hire) a builder
+  
+  #Exit the game
