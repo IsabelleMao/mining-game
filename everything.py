b240632b2e810341b30a39e5dfd1cbcd70944621
Bezeika <anjeaneemathew@gmail.com>
2017-03-07 15:57:33 -0400

    Update everything.py

--- everything.py
+++ everything.py
@@ -19,6 +19,18 @@ while True:
   
   #Mine
   
+  '''Mine generating resources. I THINK I figured this out. Got stuck in so many infinite loops (6) trying to work this out.
+  Oh, needs to generate every x seconds. Don't know how to do that.
+  Here the maximum gold the player can have is 500 and the goldMine (should) generates 100 gold every (x seconds) and adds it to gold?
+  The "+100" should show above the goldMine indicating that it's generating/working.
+  '''
+  goldMine = 0
+  gold = 0
+  while gold < 500:
+	gold += 100
+	for goldMine in range(0, 500, 100):
+		print(gold, "+100")
+    
   #Sell things?
   
   #Buy a mine
@@ -26,3 +38,5 @@ while True:
   #Buy (hire) a builder
   
   #Exit the game
+
+  
